4631ad00862ec843eb3c63c1c1804569f06cb6cd
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-07-03 14:33:48 +0200

    add carouselHome

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:kw_express/widgets/carouselHome.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('K&W Express')),
+      body: Column(
+        children: <Widget>[
+          CarouselHome(),
+        ],
+      ),
     );
   }
 }
