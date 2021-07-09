80a255a5bd5baf300a6bf4a437b6d9b92fbe1364
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-07-09 12:19:27 +0200

    create screen Favorite Resto*

---
+++ lib/screens/favoriteResto.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FavoriteResto extends StatelessWidget {
+  const FavoriteResto({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Mes Restaurants Favoris',
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 10.0,
+      ),
+    );
+  }
+}
