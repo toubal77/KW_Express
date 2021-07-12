619c09b07b1d037efbfaa9fc64b52f6cd25a6175
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-07-12 20:35:33 +0200

    add cart model and provider package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:kw_express/authScreen.dart';
+import 'package:kw_express/models/cart.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,13 +10,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'K&W Express',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) {
+        return Cart();
+      },
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'K&W Express',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: AuthScreen(),
       ),
-      home: AuthScreen(),
     );
   }
 }
