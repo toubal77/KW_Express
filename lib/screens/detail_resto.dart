f9c94abf3e6918ff30c82b215bce8cd9d09ce726
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-07-12 18:43:14 +0200

    add floating button detail resto

--- lib/screens/detail_resto.dart
+++ lib/screens/detail_resto.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kw_express/helper/icons_app.dart';
 import 'package:kw_express/home_widget.dart';
 import 'package:kw_express/models/detailRestaurant.dart';
 import 'package:kw_express/models/detailRestoMenu.dart';
 import 'package:kw_express/models/restaurant.dart';
 import 'package:kw_express/services/databases.dart';
+import 'package:speed_dial_fab/speed_dial_fab.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 // ignore: must_be_immutable
 class DetailResto extends StatefulWidget {
@@ -217,6 +220,109 @@ class _DetailRestoState extends State<DetailResto> {
           ),
         ],
       ),
+      floatingActionButton: SpeedDialFabWidget(
+        secondaryIconsList: [
+          IconsApp.trouve,
+          IconsApp.reserver,
+          IconsApp.commande,
+        ],
+        secondaryIconsText: [
+          "Trouver",
+          "Reserver",
+          "Commander",
+        ],
+        secondaryIconsOnPress: [
+          () async {
+            if (await canLaunch(widget.resDet!.map.toString())) {
+              await launch(
+                widget.resDet!.map.toString(),
+              );
+            } else {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Can\'t open google map'),
+                ),
+              );
+            }
+          },
+          () => {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Reserver une table:"),
+                      content: GestureDetector(
+                        onTap: () {
+                          launch(
+                              'tel:+213${widget.resDet!.num_tel.toString()}');
+                        },
+                        child: ListTile(
+                          leading: Icon(
+                            Icons.call,
+                            color: Colors.blue,
+                          ),
+                          title: Text(
+                            widget.resDet!.num_tel.toString(),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                )
+              },
+          () => {
+                showDialog(
+                  context: context,
+                  builder: (BuildContext context) {
+                    return AlertDialog(
+                      title: Text("Passer votre commande:"),
+                      content: Container(
+                        height: 120,
+                        child: Column(
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                launch('tel:+213${0542149642}');
+                              },
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.call,
+                                  color: Colors.blue,
+                                ),
+                                title: Text(
+                                  '0659185831',
+                                ),
+                              ),
+                            ),
+                            GestureDetector(
+                              onTap: () {
+                                launch('tel:+213${0792140427}');
+                              },
+                              child: ListTile(
+                                leading: Icon(
+                                  Icons.call,
+                                  color: Colors.blue,
+                                ),
+                                title: Text(
+                                  '0792140427',
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              },
+        ],
+        primaryIconExpand: IconsApp.floatButton,
+        secondaryBackgroundColor: Colors.red,
+        secondaryForegroundColor: Colors.white,
+        primaryBackgroundColor: Colors.red,
+        primaryForegroundColor: Colors.white,
+      ),
+
       // floatingActionButton: UnicornDialer(
       //   backgroundColor: Color.fromRGBO(255, 255, 255, 0.6),
       //   parentButtonBackground: Colors.redAccent,
