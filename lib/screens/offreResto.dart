cb056d05a8499645c7cdd377639861b6da43ab3d
toubal77 <72252709+toubal77@users.noreply.github.com>
2021-07-19 13:24:44 +0200

    offre image

--- lib/screens/offreResto.dart
+++ lib/screens/offreResto.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class OffreResto extends StatelessWidget {
   @override
@@ -16,6 +17,58 @@ class OffreResto extends StatelessWidget {
         ),
         centerTitle: true,
         elevation: 10.0,
+      ),
+      body: ListView(
+        children: [
+          Container(
+            margin: EdgeInsets.all(10),
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  blurRadius: 3,
+                  offset: Offset(0, 8),
+                ),
+              ],
+            ),
+            child: SvgPicture.asset('assets/drawable/resto_offre.svg'),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  blurRadius: 3,
+                  offset: Offset(0, 8),
+                ),
+              ],
+            ),
+            child: SvgPicture.asset('assets/drawable/resto_offre.svg'),
+          ),
+          Container(
+            margin: EdgeInsets.all(10),
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.shade500,
+                  blurRadius: 3,
+                  offset: Offset(8, 8),
+                ),
+              ],
+            ),
+            child: SvgPicture.asset('assets/drawable/resto_offre.svg'),
+          ),
+        ],
       ),
     );
   }
